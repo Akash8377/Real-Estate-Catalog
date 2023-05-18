56b19b8f08464ac34b5896aa5c368110251574b0
Akash8377 <akashchoudhary8377@gmail.com>
2023-05-18 12:57:44 +0530

    added-file

--- client/real-estate-catalog/src/App.js
+++ client/real-estate-catalog/src/App.js
@@ -1,19 +1,26 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import SearchBarPage from "./Components/SearchBarPage";
-import SignUp from './Components/screen/SignUp';
-import SignIn from './Components/screen/SignIn';
+import Signin from "./Components/screen/SignIn";
+import Signup from "./Components/screen/SignUp";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import PropertyView from "./Components/getList/PropertyView";
+// import Logout from "../logout/logout";
+import Proctected from "./Components/routes/protected.jsx";
+import AddProperty from "./Components/addProperty/main";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-
       <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/home" element={<SearchBarPage />} />
-      </Routes>
-    </BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Signin />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
+          <Route element={<Proctected />}>
+            <Route path="/home" element={<PropertyView />}></Route>
+            <Route path="/addproperty" element={<AddProperty />}></Route>
+          </Route>
+          {/* <Route path="/logout" element={<Logout />}></Route> */}
+        <Route path="*" element={<div><h1>404</h1>Page not Found</div>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
