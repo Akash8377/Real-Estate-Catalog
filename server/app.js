0d8599180b16713448486994848dc1f463a28197
Saarim Zaid <saarimzaid@gmail.com>
2023-05-18 15:50:06 +0530

    'modify-server'

--- server/app.js
+++ server/app.js
@@ -5,14 +5,6 @@ const mongoose = require('mongoose')
 const {MONGOURI} = require('./keys')
 const PORT = 5000;
 
-require('./models/user')
-mongoose.model('User')
-
-app.use(express.json())
-
-app.use(require('./routes/auth'))
-
-
 mongoose.connect(MONGOURI)
 mongoose.connection.on('connected', ()=>{
     console.log('successfully connected to mongoose')
@@ -20,6 +12,14 @@ mongoose.connection.on('connected', ()=>{
 mongoose.connection.on('error', (err)=>{
     console.log('error while connecting to mongoose', err)
 })
+
+require('./models/user')
+mongoose.model('User')
+
+app.use(express.json())
+
+app.use(require('./routes/auth'))
+
 
 app.listen(PORT, () => {
     console.log('app is running on PORT', PORT)
